3c1f9e6dbecdc1380754dde328a6cc94bbc1ed1c
aaversehere <zamazzam47@gmail.com>
2026-06-16 00:51:05 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://qanqhzpkhvyyifzikpre.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhbnFoenBraHZ5eWlmemlrcHJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MjczMTgsImV4cCI6MjA5NzEwMzMxOH0.3uWzTJCFXKZW-Y8sTO2bbnYwl4M2HkiFe1nqPpB8xF8';
+
+window.HEMODIALYSIS_CARE_SUPABASE = {
+  url: SUPABASE_URL,
+  anonKey: SUPABASE_ANON_KEY,
+};
